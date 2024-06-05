e17e36a0a974cbc8f4ad1fe77618598962f59faa
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-06-05 23:04:21 +0200

    added the library CAigerHelper

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 
 let package: Package  = Package(
     name: "CAiger",
-        products: [ .library(name: "CAiger", targets: ["CAiger"]),],
+        products: [ 
+            .library(name: "CAiger", targets: ["CAiger"]),
+            .library(name: "CAigerHelper", targets: ["CAigerHelper"]),],
     targets: [
         .target(
             name: "CAiger", 
